caec6ae35a8186a53fd89226f62bd9cf05073740
pSasha <psasha050@yandex.ru>
2022-02-05 01:05:05 +0300

    для теста

--- src/test/java/Paket1/Auto1.java
+++ src/test/java/Paket1/Auto1.java
@@ -5,14 +5,4 @@ public class Auto1 {
     String s1 = "Привет";
     String s2 = "Привет";
     String s3 = "Привет";
-    String s4 = "Привет";
-    String s5 = "Привет";
-    String s6 = "Привет";
-    String s7 = "Привет";
-    String s8 = "Привет Саша";
-    String s9 = "Привет Маша";
-    String s10 = "Привет Антон";
-    String s11 = "Привет Антон, Саша, Маша";
-	String s12 = "Привет Антон, Саша, Маша";
-	String s13 = "Привет Антон, Саша, Маша";
 }
